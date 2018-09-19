4d2f47197c47dcd89699a4b5708460e50a0d0017
Caleb Griffin <cgriffin.7622@gmail.com>
2018-09-19 15:14:54 -0400

    Text changes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var body = document.getElementById("gradient");
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-    css.textContent = "CSS style: " + body.style.background + ";";
+    css.textContent = "CSS Style - background: " + body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
